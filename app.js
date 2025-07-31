81270742a444448384d85713b6c0e866c735fab3
Shehwar Maryam <shehwarm93@gmail.com>
2025-07-31 12:58:31 -0700

    Refactor app.js to implement day-based logging using switch statement

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-let str = "apple";
-length = str.length; 
-firstLetter = str[0];
-console.log("length of the string is " + length);
+let day= "monday";
 
-console.log("first letter of the string is " + firstLetter); 
-
-if(length > 3 && firstLetter === 'a') {
-    console.log("Good string");
-}
-else {
-    console.log("Bad string");
+switch(day) {
+    case "monday": 
+        console.log("Its monday");
+        break;
+    case "tuesday":
+        console.log("Its tuesday");
+        break;
+    case "wednesday":
+        console.log("Its wednesday");
+        break;
+    case "thursday":
+        console.log("Its thursday");
+        break;
+    case "friday":    
+        console.log("Its friday");
+        break;                                          
+    case "saturday":
+        console.log("Its saturday");   
+        break;
+    case "sunday":
+        console.log("Its sunday");
+        break;
+    default:
+        console.log("Invalid day");
 }
