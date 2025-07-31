0aacae0a6c0d4c0c5bd82c60b76d7bd065ffaa34
Shehwar Maryam <shehwarm93@gmail.com>
2025-07-31 13:04:37 -0700

    Remove day-based logging switch statement and prompt for number input

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-let day= "monday";
 
-switch(day) {
-    case "monday": 
-        console.log("Its monday");
-        break;
-    case "tuesday":
-        console.log("Its tuesday");
-        break;
-    case "wednesday":
-        console.log("Its wednesday");
-        break;
-    case "thursday":
-        console.log("Its thursday");
-        break;
-    case "friday":    
-        console.log("Its friday");
-        break;                                          
-    case "saturday":
-        console.log("Its saturday");   
-        break;
-    case "sunday":
-        console.log("Its sunday");
-        break;
-    default:
-        console.log("Invalid day");
-}
+
+prompt("enter a number between 1 and 7");
