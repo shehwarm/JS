fde55bc49e2bee9b6e7ba378839ed67abb7a6e17
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-01 13:41:54 +0500

    Refactor string manipulation in app.js to trim and convert to uppercase

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-let str ="Apna College";
-
-newstr = str.trim();
-str1= str.toUpperCase();
-str2= str1.toLowerCase();   
-str3 = str.indexOf("College");
-
-console.log(newstr); // Output: "Apna College"
-console.log(str1); // Output: "APNA COLLEGE"
-console.log(str2); // Output: "apna college"
-console.log(str3); // Output: 5 (index of "College" in the string)
+let str ="  Apna College";
+let newMsg = msg.trim();
+console.log(newMsg);
+newMsg=newMsg.toUpperCase();
+console.log(newMsg);
