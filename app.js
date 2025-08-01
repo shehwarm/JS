4e4cd718d5d9589246fd9c73e7a2ad4b9eeda69e
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-01 13:33:11 +0500

    Fix variable assignment in string manipulation logic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let str =" Apna College";
 
 newstr = str.trim();
 str1= str.toUpperCase();
-str2= str2.toLowerCase();   
+str2= str1.toLowerCase();   
 
 console.log(newstr); // Output: "Apna College"
 console.log(str1); // Output: "APNA COLLEGE"
