c7395faa59b56e40d6531d259705e0b859794615
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-01 13:38:56 +0500

    Add indexOf method to find position of "College" in string

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ let str =" Apna College";
 newstr = str.trim();
 str1= str.toUpperCase();
 str2= str1.toLowerCase();   
+str3 = str.indexOf("College");
 
 console.log(newstr); // Output: "Apna College"
 console.log(str1); // Output: "APNA COLLEGE"
 console.log(str2); // Output: "apna college"
+console.log(str3); // Output: 5 (index of "College" in the string)
