5854ea5f2c829bc03379c782e93cc33d90de3d75
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-07 11:18:48 +0500

    Replace fruit array with heroes array and update loop to print hero names

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+let heroes = [
+     ["Ironman", "Thor", "Hulk"],
+     ["Black Widow", "Captain America", "Hawkeye"]
+  ]
 
-for(let i = 0; i < fruits.length; i++) {
-
-  console.log(fruits[i]);
+for(let i = 0; i < heroes.length; i++) {
+  for(let j = 0; j < heroes[i].length; j++) {
+    console.log(heroes[i][j]);
+  }
 }
