805f3a4ee4b17d677ed7f555de97446e69dd2e62
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-07 12:02:16 +0500

    Refactor task listing in To Do App to include task indices for better clarity

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ while(true){
   
   if( req == "list"){
     console.log("Your tasks are: ");
-    for(task of todo){
-      console.log(task);
+    for(let i=0; i<todo.length; i++){
+      console.log(i, todo[i]);
     }
   }  else if(req == "add"){
     let task = prompt("Please enter your task");
     todo.push(task);
     console.log("task added successfully");
   }
+   req= prompt("Please enter your request");
 }
