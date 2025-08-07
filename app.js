60fb0c6fd7e5f07de692c17dba24bcad4ba828f0
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-07 11:52:59 +0500

    Fix prompt handling to ensure tasks are listed and added correctly in To Do App

--- app.js
+++ app.js
@@ -8,4 +8,15 @@ while(true){
     console.log("quitting app");
     break;
   }
+  
+  if( req == "list"){
+    console.log("Your tasks are: ");
+    for(task of todo){
+      console.log(task);
+    }
+  }  else if(req == "add"){
+    let task = prompt("Please enter your task");
+    todo.push(task);
+    console.log("task added successfully");
+  }
 }
