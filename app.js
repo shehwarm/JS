c344b946dbf02988c1aff110a053d01b988027a9
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-08 11:36:10 +0500

    Refactor app.js: replace hello and printName functions with poem function for creative output

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-function hello(){
-    console.log("Hello, World!");
+function poem(){
+    console.log("Roses are red, violets are blue,");
+    console.log("This is a simple poem, just for you.");
+    console.log("In the world of code, we find our way,");
+    console.log("Creating magic with each passing day.");
 }
 
-hello();
-
-function printName() {
-    console.log("My name is John Doe.");
-}
-
-printName();
+poem();
