9d92ae025adffbfc1d72aef73727a67e55c235a9
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-09 15:24:48 +0500

    arrow function

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-console.log("Hello, World!");
-
-try{
-    console.log(a);
-} catch {
-    console.error("An error occurred: ", e);
-}
-
-console.log("This is a test message.");
-console.log("This is another test message.");
+const sum = (a, b) => { console.log(a + b); };
+sum(5, 10);
