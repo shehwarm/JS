dcc48077721c3ed930d99c4dbf10a96d8a02d0bb
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-11 12:07:40 +0500

    minimum through reduce method

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
 let arr = [10, 15, 20, 25, 30, 35, 40, 45, 50];
 
-let multiple = arr.every((el)=>{
-    return  el % 10 == 0;
-})
+let min = arr.reduce((acc, curr) => {
+    if(curr < acc){
+        return curr;    
+    } else {
+        return acc;
+    }
+});
 
-console.log(multiple); 
-// This code checks if every element in the array is a multiple of 10.
+console.log(min);
