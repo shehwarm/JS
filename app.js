135c0ad32124ac7087cad4446eac7c8963866a93
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-11 11:04:22 +0500

    Every Mehod

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let arr = [1, 2, 3, 4, 5];
 
-let even = arr.filter((el)=> {
+
+let even = [1, 2, 3, 4, 5].every((el)=> {
     return el% 2 == 0
 });
+
+console.log(even); // false
+
+let odd = [3,5,7].every((el)=>{
+    return el % 2 == 1
+});
+
+console.log(odd); // true
