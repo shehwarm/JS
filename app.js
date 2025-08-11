25b8eda817cdff431966fdbb2c2b87621596dddb
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-11 11:58:54 +0500

    Multiple of 10 through every method.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-let arr = [1,2,3,4,5,6,7,8,9];
+let arr = [10, 15, 20, 25, 30, 35, 40, 45, 50];
 
-let max = arr.reduce((acc, curr) =>{
-    if(curr > acc){
-        return curr;
-    } else {
-        return max;
-    }
-});
- console.log(max); // 9
+let multiple = arr.every((el)=>{
+    return  el % 10 == 0;
+})
+
+console.log(multiple); 
+// This code checks if every element in the array is a multiple of 10.
