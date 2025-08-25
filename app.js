c46fc3a86aef87ee9bd5ee9803ac07433b80578c
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-25 21:05:25 +0500

    program to strip leading and trailing spaces from a string

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let str = "ShehwarMaryam";
-let idx= 0;
-
-if(str[idx] == str[idx].toUpperCase()){
-    console.log("character is upper case");
-} else{
-    console.log("character is  lowercase");
-}
+let str = prompt("enter a string");
+console.log(`orignal string is  ${str}`);
+console.log(`string without spaces = ${str.trim()}`);
