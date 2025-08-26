8640789df749e18a17b56d393ae86e4992b507b8
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-26 19:17:17 +0500

    program to find the no of digits in a number

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-let arr =[1,2,3,4,5,2];
-let num = 2;
+let num = 2345642;
 
-for(let i=0; i<arr.length; i++){
-    if(arr[i] == num) {
-        arr.splice(i,1);
-    }
+let count = 0;
+
+let copy = num;
+
+while(copy>0){
+    count++;
+    copy = Math.floor(copy/10);
 }
 
-console.log(arr);
-
+console.log(count);
