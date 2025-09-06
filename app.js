f95c10dfb11199e09c288cefca26871ea6267710
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-05 20:11:05 -1200

    Arrow Function

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
+const multiply = (x, y) => x * y;
 
-
-function square(num) {
-    return num * num;
-}
-
-function sumOfSquares(a, b) {
-    return square(a) + square(b);
-}
-
-console.log(sumOfSquares(3, 4)); // 9 + 16 = 25
+console.log(multiply(4, 5)); // 20
