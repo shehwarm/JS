9211bb43655176c1a353221e66ea7f658734c53a
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-05 20:10:25 -1200

    Function Returning Another Function (Closure)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let start = 100;
-let end = 300;
 
-function generateRandom(start,end){
-    let diff = end - start;
-    return Math.floor(Math.random() * diff ) + start;
+
+function makeMultiplier(factor) {
+    return function(num) {
+        return num * factor;
+    };
 }
 
+const double = makeMultiplier(2);
+const triple = makeMultiplier(3);
+
+console.log(double(5)); // 10
+console.log(triple(5)); // 15
