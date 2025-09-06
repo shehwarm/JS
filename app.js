0668c3a9088be19598f1de61ee62df91da6b6fa3
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-06 13:13:43 +0500

    Arrow Function with Array Methods

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const subtract = function(a, b) {
-    return a - b;
-};
+const numbers = [2, 4, 6, 8, 10];
 
-console.log(subtract(15, 7)); // 8
+const squares = numbers.map(num => num * num);
+
+console.log(squares); // [4, 16, 36, 64, 100]
