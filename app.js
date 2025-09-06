4240ea14f1e97f51b02e3392cb2f9f23c4b7a467
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-05 20:11:37 -1200

    Function with Default Parameter

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const multiply = (x, y) => x * y;
+function add(a, b = 5) {
+    return a + b;
+}
 
-console.log(multiply(4, 5)); // 20
+console.log(add(10));   // uses default b = 5 → 15
+console.log(add(10, 20)); // overrides default → 30
