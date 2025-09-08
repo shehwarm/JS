27477ffa2e2d69ffde5513895d5d67580425cf7b
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-08 19:31:17 +0500

    Calculator using Functions

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function sumAll(...nums) {
-    return nums.reduce((acc, curr) => acc + curr, 0);
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return b !== 0 ? a / b : "Error: Division by zero";
 }
 
-console.log(sumAll(1, 2, 3, 4, 5)); // 15
+console.log("Add:", add(10, 5));
+console.log("Subtract:", subtract(10, 5));
+console.log("Multiply:", multiply(10, 5));
+console.log("Divide:", divide(10, 5));
