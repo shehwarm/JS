f125c52ee5e359bcdb4c400269195aa9faf618a0
Shehwar Maryam <shehwarm93@gmail.com>
2025-09-12 21:12:04 +0500

    Refactor fruit array manipulation: replace pop with push and update console log

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-function isPalindrome(str) {
-    let reversed = str.split("").reverse().join("");
-    return str === reversed;
-}
-
-console.log(isPalindrome("madam"));  // true
-console.log(isPalindrome("hello"));  // false
+let fruits = ["apple", "banana", "mango"];
+fruits.pop();
+console.log(fruits); // ["apple", "banana"]
